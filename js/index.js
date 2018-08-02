e74583da1553a52cd8ca1cfa376fe4b1b49ba1a8
Backend Mac <williameduardomeza@gmail.com>
2018-08-02 13:47:22 -0500

    add google auth sdk

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-console.log(firebase);
+var btnLogin = document.getElementById("btnLogin");
+
+console.log(btnLogin);
+
+firebase.auth().onAuthStateChanged(function (user) {
+    console.log(user);
+    if (user) {
+        console.log("tenemos usuario");
+    }
+    else{
+        console.log("no tenemos usuario");
+    }
+});
+
+btnLogin.addEventListener("click", function () {
+    event.preventDefault();
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().signInWithPopup(provider)
+    .then(function (datosUsuario) {
+        console.log(datosUsuario);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+});
