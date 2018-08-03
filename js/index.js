00ad019f74ce3b67ecd488d9ec285a0258c5d39e
Backend Mac <williameduardomeza@gmail.com>
2018-08-03 11:26:39 -0500

    handle auth with Google

--- js/index.js
+++ js/index.js
@@ -1,26 +1,33 @@
-var btnLogin = document.getElementById("btnLogin");
-
-console.log(btnLogin);
-
-firebase.auth().onAuthStateChanged(function (user) {
+const btnLogin = document.getElementById('btnLogin');
+const btnLogout = document.getElementById('btnLogout');
+firebase.auth().onAuthStateChanged((user) => {
     console.log(user);
     if (user) {
-        console.log("tenemos usuario");
-    }
-    else{
-        console.log("no tenemos usuario");
+        btnLogin.style.display = 'none';
+        btnLogout.style.display = 'block';
+    } else {
+        btnLogin.style.display = 'block';
+        btnLogout.style.display = 'none';
     }
 });
 
-btnLogin.addEventListener("click", function () {
-    event.preventDefault();
-    var provider = new firebase.auth.GoogleAuthProvider();
+// Event Listeners
+
+btnLogout.addEventListener('click', (evt) => {
+    btnLogin.style.display = 'block';
+    btnLogout.style.display = 'none';
+    firebase.auth().signOut();
+});
+
+btnLogin.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    firebase.auth().signInWithPopup(provider)
-    .then(function (datosUsuario) {
-        console.log(datosUsuario);
-    })
-    .catch(function (error) {
+    firebase.auth().signInWithPopup(provider).then((data) => {
+        btnLogin.style.display = 'none';
+        btnLogout.style.display = 'block';
+        //console.log(data);
+    }).catch((error) => {
         console.log(error);
-    })
+    });
 });
