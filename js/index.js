834f2132efa67802e7a54dca56ca781932b9568f
Backend Mac <williameduardomeza@gmail.com>
2018-08-06 14:18:17 -0500

    Read a single record from the Database (Realtime)

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
 const btnLogin = document.getElementById('btnLogin');
 const btnLogout = document.getElementById('btnLogout');
-
+var ref = firebase.database().ref("usuario");
 var usuario = {};
+
+function leerInformacion (user) {
+    ref.child(user.uid).once('value', function (data) {
+        console.log(data.val());
+    });
+}
+
 firebase.auth().onAuthStateChanged((user) => {
-    console.log(user);
     if (user) {
         btnLogin.style.display = 'none';
         btnLogout.style.display = 'block';
+        leerInformacion(user);
     } else {
         btnLogin.style.display = 'block';
         btnLogout.style.display = 'none';
@@ -14,7 +21,6 @@ firebase.auth().onAuthStateChanged((user) => {
 });
 
 // Event Listeners
-
 btnLogout.addEventListener('click', (evt) => {
     btnLogin.style.display = 'block';
     btnLogout.style.display = 'none';
@@ -33,12 +39,12 @@ btnLogin.addEventListener('click', (evt) => {
             email: data.user.email,
             uid: data.user.uid
         }
-        agregarUsuario(usuario);
+        agregarUsuario(usuario, usuario.uid);
     }).catch((error) => {
         console.log(error);
     });
 });
-function agregarUsuario(usuario) {
-    var ref = firebase.database().ref("usuario");
-    ref.push(usuario);
+
+function agregarUsuario(usuario, uid) {
+    ref.child(uid).update(usuario);
 }
