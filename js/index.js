8c315a6beb6fe3ada25dec99adcab71d2c7b3d99
Backend Mac <williameduardomeza@gmail.com>
2018-08-13 09:54:42 -0500

    add guitars and get documents from DB

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const btnLogin = document.getElementById('btnLogin');
 const btnLogout = document.getElementById('btnLogout');
 var ref = firebase.database().ref("usuario");
+var refGuitarras = firebase.database().ref("guitarras");
 var usuario = {};
 
 function leerInformacion (user) {
@@ -29,8 +30,11 @@ btnLogout.addEventListener('click', (evt) => {
 
 btnLogin.addEventListener('click', (evt) => {
     evt.preventDefault();
-    const provider = new firebase.auth.FacebookAuthProvider();
-    provider.addScope('public_profile');
+    // Facebook Auth
+    // const provider = new firebase.auth.FacebookAuthProvider();
+    // provider.addScope('public_profile');
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
     firebase.auth().signInWithPopup(provider).then((data) => {
         btnLogin.style.display = 'none';
         btnLogout.style.display = 'block';
@@ -48,3 +52,17 @@ btnLogin.addEventListener('click', (evt) => {
 function agregarUsuario(usuario, uid) {
     ref.child(uid).update(usuario);
 }
+
+function leerGuitarras() {
+    refGuitarras.child("normal").on("value", function (guitarras) {
+        console.log(guitarras.val());
+    });
+}
+leerGuitarras();
+
+function leerGuitarrasVIP() {
+    refGuitarras.child("vip").on("value", function (guitarras) {
+        console.log(guitarras.val());
+    });
+}
+leerGuitarrasVIP();
